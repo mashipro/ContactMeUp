1dfd7397925be0b526b6047702284b07d8d054bd
Mashipro <captain.neon99@google.com>
2023-08-05 12:26:42 +0700

    Navigation is implemented

---
+++ src/Screens/SplashScreen.tsx
@@ -0,0 +1,22 @@
+import {Text, View} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {IMainNavigatorPropTypes} from '../Routes/MainNavigationTypes';
+
+const SplashScreen: FC<IMainNavigatorPropTypes<'SplashScreen'>> = ({
+  navigation,
+}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.push('ContactListScreen');
+    }, 1000);
+    return () => {};
+  }, [navigation]);
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 100, fontWeight: 'bold'}}>SplashScreen</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
