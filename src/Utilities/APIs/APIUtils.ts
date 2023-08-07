e58366c26cf2631fcb750cb14133352a07c72211
Mashipro <captain.neon99@google.com>
2023-08-07 11:45:26 +0700

    fix api types

--- src/Utilities/APIs/APIUtils.ts
+++ src/Utilities/APIs/APIUtils.ts
@@ -1,4 +1,4 @@
-import {EndpointPool} from './EndpointPool';
+import EndpointPool from './EndpointPool';
 
 enum EndpointMethod {
   get,
@@ -22,18 +22,3 @@ export type Endpoint = (typeof EndpointPool)[number]['endpoint'];
 export const getEndpoint = (endpoint: Endpoint) => {
   return EndpointPool.find(item => item.endpoint === endpoint);
 };
-
-// /**
-//  * Endpoint Array Creator
-//  * @param args
-//  * @returns EndpointTypes[]
-//  */
-
-function createEndpointArray<
-  T extends readonly EndpointTypes[] & Array<{endpoint: V}>,
-  V extends string,
->(...args: T) {
-  return args;
-}
-
-export default createEndpointArray;
